591df69c961bdb3cdbb18ec4def6c2493189c26a
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-03-06 14:14:54 +0300

    Update to borsch v2

--- cmake/pypi_process.py
+++ cmake/pypi_process.py
@@ -12,8 +12,9 @@ with open(sys.argv[1]) as data_file:
     data = json.load(data_file)
 
     version = data['info']['version']
+    name = data['info']['name']
     for i in data['releases'][version]:
-        if i['url'].endswith('tar.gz'):
+        if i['url'].endswith('tar.gz') or i['url'].endswith('zip'):
             download_url = i['url']
             date = i['upload_time'].replace('T', ' ')
             break
@@ -23,7 +24,8 @@ with open(sys.argv[1]) as data_file:
 
     version_file_name = os.path.join(os.path.dirname(sys.argv[1]), 'version.str')
     version_file = open(version_file_name, 'w')
-    version_file.write("%s\n%s" % (version, date))
+    pack_name = "{}-{}-{}".format(name, version, sys.argv[2])
+    version_file.write("{}\n{}\n{}".format(version, date, pack_name))
     version_file.close()
 
-print download_url + ';' + version
+print download_url + ';' + version + ';' + pack_name
